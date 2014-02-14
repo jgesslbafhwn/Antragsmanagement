da270df765023e5af4544ec411b0d3261b18cd85
Your_Wakanda_Developer_Name_Here <WakandaDeveloper@wakanda.org>
2014-02-14 14:17:15 +0100

    #5 Combobox

--- Antragsmanagement/WebFolder/components/programm.waComponent/programm.js
+++ Antragsmanagement/WebFolder/components/programm.waComponent/programm.js
@@ -15,13 +15,23 @@ function constructor (id) {
 	this.load = function (data) {// @lock
 		
 	// @region namespaceDeclaration// @startlock
+	var programmEvent = {};	// @dataSource
 	var imageButton3 = {};	// @buttonImage
 	// @endregion// @endlock
 
 	// eventHandlers// @lock
 
+	programmEvent.onCollectionChange = function programmEvent_onCollectionChange (event)// @startlock
+	{// @endlock
+		if(sources.programm.length > 0)
+			$$(getHtmlId('container2')).show();
+		else
+			$$(getHtmlId('container2')).hide();
+	};// @lock
+
 	imageButton3.click = function imageButton3_click (event)// @startlock
 	{// @endlock
+
 		// Add your code here
 		var vis = $$(getHtmlId('container2')).isVisible();
 		if (!vis)
@@ -29,11 +39,12 @@ function constructor (id) {
 	};// @lock
 
 	// @region eventManager// @startlock
+	WAF.addListener(this.id + "_programm", "onCollectionChange", programmEvent.onCollectionChange, "WAF");
 	WAF.addListener(this.id + "_imageButton3", "click", imageButton3.click, "WAF");
 	// @endregion// @endlock
 
 	};// @lock
-
+		
 }// @startlock
 return constructor;
 })();// @endlock
